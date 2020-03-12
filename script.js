36b4be28ba6d6a21d3c9918ae5a168ca2c6e0332
ahmed fawzy <42780497+fawzy2010eng@users.noreply.github.com>
2020-03-12 14:47:36 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+//we have an array of squares whare the coordinates of the array is it's index in the array and the height is the value
+//we loop over every square adding the upper and the lower area then loop _for every squres_ through it's four sides 
+//if there's no square on the nth direction or the height is greater then this area is 0
+//we get the sum of the areas for every square theb return the sum of the all squares
